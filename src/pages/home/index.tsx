a24087f741406f0a6d69dd024eaea6e6d1d47ae3
abdulrehmanazizf5 <abdulrehmanaziz5f@gmail.com>
2026-03-19 16:05:15 +0500

    feat: implement home page structure with navigation, hero section, product list, and footer.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import Navbar from "../../layout/navbar";
+import Nav from "../../layout/navbar";
 import HeroSecCrousel from "./hero-sec-crousel/";
 import Products from "./products";
 import Footer from "../../layout/footer";
@@ -7,11 +7,9 @@ const index = () => {
   return (
     <>
       <header>
-        <nav>
-          <Navbar
-            style={{ position: "fixed", width: "100%", top: 0, zIndex: 1000 }}
-          />
-        </nav>
+        <Nav
+          style={{ position: "fixed", width: "100%", top: 0, zIndex: 1000 }}
+        />
         <HeroSecCrousel />
       </header>
       <Products />
